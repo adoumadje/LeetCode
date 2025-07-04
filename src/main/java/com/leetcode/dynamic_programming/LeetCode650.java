d3e8466df9de38917f7f127e48b7d8a52a80b205
adoumadje <adoumadjecaleb@gmail.com>
2025-07-04 20:57:46 +0200

    650. 2 Keys Keyboard - 1 approach

---
+++ src/main/java/com/leetcode/dynamic_programming/LeetCode650.java
@@ -0,0 +1,40 @@
+package com.leetcode.dynamic_programming;
+
+import java.util.Arrays;
+
+public class LeetCode650 {
+    boolean[] prime;
+
+    public int minSteps(int n) {
+        this.prime = getPrimes(n);
+        int[] dp = new int[n+1];
+        for(int i = 2; i <= n; ++i) {
+            for(int j = 2; j <= i; ++j) {
+                if(prime[j] && i % j == 0) {
+                    dp[i] = j + dp[i/j];
+                }
+            }
+        }
+        return dp[n];
+    }
+
+    private int primef(int num) {
+        for(int i = num; i >= 0; --i) {
+            if(prime[i] && num % i == 0) return i;
+        }
+        return -1;
+    }
+
+    private boolean[] getPrimes(int n) {
+        boolean[] primes = new boolean[n+1];
+        Arrays.fill(primes, true);
+        primes[0] = false;
+        for(int i = 2; i <= n; ++i) {
+            if(!primes[i]) continue;
+            for(int j = i*2; j <= n; j += i) {
+                primes[j] = false;
+            }
+        }
+        return primes;
+    }
+}
