c4e712fda23f414489eecb42b78604cf884abb8c
adoumadje <adoumadjecaleb@gmail.com>
2025-07-04 18:08:50 +0200

    647. Palindromic Substrings

---
+++ src/main/java/com/leetcode/dynamic_programming/LeetCode646.java
@@ -0,0 +1,22 @@
+package com.leetcode.dynamic_programming;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class LeetCode646 {
+    public int findLongestChain(int[][] pairs) {
+        Comparator<int[]> comparator = (a, b) -> Integer.compare(a[0], b[0]);
+        Arrays.sort(pairs, comparator);
+        int end = -1001;
+        int count = 0;
+        for(int[] p: pairs) {
+            if(p[0] > end) {
+                count++;
+                end = p[1];
+            } else {
+                end = Math.min(end, p[1]);
+            }
+        }
+        return count;
+    }
+}
---
+++ src/main/java/com/leetcode/dynamic_programming/LeetCode647.java
@@ -0,0 +1,33 @@
+package com.leetcode.dynamic_programming;
+
+public class LeetCode647 {
+    char[] s;
+    Integer[][] mem;
+    public int countSubstrings(String s) {
+        this.s = s.toCharArray();
+        int n = s.length();
+        mem = new Integer[n][n];
+        int res = 0;
+        for(int i = 0; i < n; ++i) {
+            res += 1 + palindrome(i-1, i+1)
+                    + palindrome(i, i+1);
+        }
+        return res;
+    }
+
+    private int palindrome(int l, int r) {
+        int n = s.length;
+        if(l < 0 || r >= n) return 0;
+        if(mem[l][r] != null) return mem[l][r];
+        int num = 0;
+        int ol = l, or = r;
+        while(l >= 0 && r < n) {
+            if(s[l--] == s[r++]) {
+                num++;
+            } else {
+                break;
+            }
+        }
+        return mem[ol][or] = num;
+    }
+}
