1741eb3a855b3ff1c7cb6fa3a79290aa0c40c7dd
adoumadje <adoumadjecaleb@gmail.com>
2025-07-12 15:58:43 +0200

    787. Cheapest Flights Within K Stops

---
+++ src/main/java/com/leetcode/dynamic_programming/LeetCode787.java
@@ -0,0 +1,24 @@
+package com.leetcode.dynamic_programming;
+
+import java.util.Arrays;
+
+public class LeetCode787 {
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        int[] prices = new int[n];
+        Arrays.fill(prices, Integer.MAX_VALUE);
+        prices[src] = 0;
+        for(int i = 0; i <= k; ++i) {
+            int[] temp = prices.clone();
+            for(int[] flight: flights) {
+                int from = flight[0];
+                int to = flight[1];
+                int price = flight[2];
+                if(prices[from] != Integer.MAX_VALUE) {
+                    temp[to] = Math.min(temp[to], prices[from] + price);
+                }
+            }
+            prices = temp;
+        }
+        return prices[dst] == Integer.MAX_VALUE ? -1 : prices[dst];
+    }
+}
