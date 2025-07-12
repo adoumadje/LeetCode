b3490dde2a525feaab10a78cd84920ecb0b8765e
adoumadje <adoumadjecaleb@gmail.com>
2025-07-12 21:17:58 +0200

    788. Rotated Digits

---
+++ src/main/java/com/leetcode/dynamic_programming/LeetCode788.java
@@ -0,0 +1,26 @@
+package com.leetcode.dynamic_programming;
+
+import java.util.Arrays;
+
+public class LeetCode788 {
+    public int rotatedDigits(int n) {
+        int count = 0;
+        for(int i = 0; i <= n; ++i) {
+            count += checkDigits(i);
+        }
+        return count;
+    }
+
+    private int checkDigits(int num) {
+        int counter = 0;
+        while (num > 0) {
+            int d = num % 10;
+            if(d == 3 || d == 4 || d == 7) return 0;
+            if(d == 2 || d == 5 || d == 6 || d == 9) {
+                ++counter;
+            }
+            num /= 10;
+        }
+        return counter > 0 ? 1 : 0;
+    }
+}
