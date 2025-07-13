b2519755e9cccec7d2700c7fc3214f2343748428
adoumadje <adoumadjecaleb@gmail.com>
2025-07-13 21:18:05 +0200

    790. Domino and Tromino Tiling - tabulation bottom up

--- src/main/java/com/leetcode/dynamic_programming/LeetCode790.java
+++ src/main/java/com/leetcode/dynamic_programming/LeetCode790.java
@@ -1,31 +1,16 @@
 package com.leetcode.dynamic_programming;
 
 public class LeetCode790 {
-    Integer[][] mem;
-    int MOD = (int)1e9 + 7;
+    int MOD = (int)(1e9 + 7);
     public int numTilings(int n) {
-        mem = new Integer[n+1][n+1];
-        return dfs(n, n);
-    }
-
-    private int dfs(int r1, int r2) {
-        if(r1 == 0 && r2 == 0) return 1;
-        if(r1 < 0 || r2 < 0) return 0;
-        if(mem[r1][r2] != null) return mem[r1][r2];
-
-        long res = 0;
-        if(r1 == r2) {
-            res += dfs(r1-2, r2-2);
-            res += dfs(r1-1, r2-1);
-            res += dfs(r1-1, r2-2);
-            res += dfs(r1-2, r2-1);
-        } else if (r1 > r2) {
-            res += dfs(r1-2, r2);
-            res += dfs(r1-2, r2-1);
-        } else {
-            res += dfs(r1, r2-2);
-            res += dfs(r1-1, r2-2);
+        if(n < 3) return n;
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i = 3; i <= n; ++i) {
+            dp[i] = (2 * dp[i-1] + dp[i-3]) % MOD;
         }
-        return mem[r1][r2] = (int) (res % MOD);
+        return dp[n];
     }
 }
