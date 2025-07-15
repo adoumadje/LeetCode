1eec7bca8a804c816f5350d4be16805de188e70b
adoumadje <adoumadjecaleb@gmail.com>
2025-07-15 17:51:15 +0200

    792. Number of Matching Subsequences - finished

--- src/main/java/com/leetcode/dynamic_programming/medium/LeetCode792.java
+++ src/main/java/com/leetcode/dynamic_programming/medium/LeetCode792.java
@@ -1,9 +1,8 @@
 package com.leetcode.dynamic_programming.medium;
 
-import java.util.LinkedList;
+import java.util.*;
 
 public class LeetCode792 {
-
     class Word {
         String word;
         int index;
@@ -13,8 +12,8 @@ public class LeetCode792 {
             this.index = 0;
         }
 
-        public char currentChar() {
-            return word.charAt(index);
+        public int getPosition() {
+            return word.charAt(index) - 'a';
         }
 
         public boolean isSubsequence() {
@@ -23,39 +22,34 @@ public class LeetCode792 {
     }
 
     public int numMatchingSubseq(String s, String[] words) {
-        int n = s.length();
-        LinkedList<Word>[] lists = new LinkedList[n];
-        for(int i = 0; i < n; ++i) {
-            lists[i] = new LinkedList<>();
+        int n = s.length(), alph = 26;
+        LinkedList<Word>[] dp = new LinkedList[alph];
+
+        for(int i = 0; i < alph; ++i) {
+            dp[i] = new LinkedList<>();
         }
+
         for(String w: words) {
-            for(int i = 0; i < n; ++i) {
-                if(w.charAt(0) == s.charAt(i)) {
-                    lists[i].add(new Word(w));
-                    break;
-                }
-            }
+            Word word = new Word(w);
+            dp[word.getPosition()].addLast(word);
         }
 
         int counter = 0;
 
-        for(int i = 0; i < n; ++i) {
-            LinkedList<Word> list = lists[i];
-            while (!list.isEmpty()) {
-                Word word = list.removeFirst();
-                word.index++;
+        for(char c: s.toCharArray()) {
+            int i = c - 'a';
+            int size = dp[i].size();
+            for(int j = 0; j < size; ++j) {
+                Word word = dp[i].removeFirst();
+                ++word.index;
                 if(word.isSubsequence()) {
                     ++counter;
-                    continue;
-                }
-                char c = word.currentChar();
-                for(int j = i+1; j < n; ++j) {
-                    if(c == s.charAt(j)) {
-                        lists[j].addLast(word);
-                    }
+                } else {
+                    dp[word.getPosition()].addLast(word);
                 }
             }
         }
+
         return counter;
     }
 }
