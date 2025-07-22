5af90195bf748361e50c00328c820691be58f948
adoumadje <adoumadjecaleb@gmail.com>
2025-07-22 20:57:36 +0200

    873. Length of Longest Fibonacci Subsequence

--- src/main/java/com/leetcode/dynamic_programming/medium/LeetCode873.java
+++ src/main/java/com/leetcode/dynamic_programming/medium/LeetCode873.java
@@ -1,4 +1,25 @@
 package com.leetcode.dynamic_programming.medium;
 
 public class LeetCode873 {
+    public int lenLongestFibSubseq(int[] arr) {
+        int n = arr.length;
+        int[][] dp = new int[n][n];
+        int res = 0;
+        for(int i = 2; i < n; ++i) {
+            int l = 0, r = i-1;
+            while(l < r) {
+                int sum = arr[l] + arr[r];
+                if(sum == arr[i]) {
+                    dp[r][i] = 1 + dp[l][r];
+                    res = Math.max(res, dp[r][i] + 2);
+                    l++;
+                } else if (sum > arr[i]) {
+                    r--;
+                } else {
+                    l++;
+                }
+            }
+        }
+        return res;
+    }
 }
