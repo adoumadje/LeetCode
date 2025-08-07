ce04e61736b1a5ffc1be3455cf9117e5c89a3dde
adoumadje <adoumadjecaleb@gmail.com>
2025-08-07 15:21:27 +0200

    1027. Longest Arithmetic Subsequence - optimized

--- src/main/java/com/leetcode/dynamic_programming/medium/LeetCode1027.java
+++ src/main/java/com/leetcode/dynamic_programming/medium/LeetCode1027.java
@@ -1,25 +1,21 @@
 package com.leetcode.dynamic_programming.medium;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class LeetCode1027 {
     public int longestArithSeqLength(int[] nums) {
         int n = nums.length;
-        Map<Integer, Integer>[] dp = new Map[n];
-        dp[0] = new HashMap<>();
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+        for(int x: nums) {
+            min = Math.min(min, x);
+            max = Math.max(max, x);
+        }
+        int universe = max - min;
+        int[][] dp = new int[n][2 * universe + 1];
         int res = 0;
         for(int i = 1; i < n; ++i) {
-            dp[i] = new HashMap<>();
             for(int j = i-1; j >= 0; --j) {
-                int diff = nums[i] - nums[j];
-                int finalJ = j;
-                dp[i].compute(diff, (k, v) -> {
-                    int prev = dp[finalJ].getOrDefault(diff, 0);
-                    v = v == null ? prev + 1 : Math.max(v, prev + 1);
-                    return v;
-                });
-                res = Math.max(res, dp[i].get(diff) + 1);
+                int d = nums[i] - nums[j] + universe;
+                dp[i][d] = Math.max(dp[i][d], dp[j][d] + 1);
+                res = Math.max(res, dp[i][d] + 1);
             }
         }
         return res;
