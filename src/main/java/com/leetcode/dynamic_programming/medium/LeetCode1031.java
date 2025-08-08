3a12f99d4599fae650b2ee5833fbcd7ecb531529
adoumadje <adoumadjecaleb@gmail.com>
2025-08-08 16:43:51 +0200

    1031. Maximum Sum of Two Non-Overlapping Subarrays -  optimal

--- src/main/java/com/leetcode/dynamic_programming/medium/LeetCode1031.java
+++ src/main/java/com/leetcode/dynamic_programming/medium/LeetCode1031.java
@@ -3,28 +3,23 @@ package com.leetcode.dynamic_programming.medium;
 public class LeetCode1031 {
     public int maxSumTwoNoOverlap(int[] nums, int firstLen, int secondLen) {
         int n = nums.length;
-        int[] first = buildDP(nums, firstLen);
-        int[] second = buildDP(nums, secondLen);
+        int[] dp = new int[n+1];
+        for(int i = 1; i <= n; ++i) {
+            dp[i] = dp[i-1] + nums[i-1];
+        }
         int res = 0;
-        for(int i = 0; i < n-firstLen+1; ++i) {
-            for(int j = 0; j < n-secondLen+1; ++j) {
-                if(j + secondLen - 1 < i || j > i + firstLen - 1) {
-                    res = Math.max(res, first[i] + second[j]);
-                }
-            }
+        int maxFirst = 0;
+        for(int i = firstLen; i <= n-secondLen; ++i) {
+            maxFirst = Math.max(maxFirst, dp[i] - dp[i-firstLen]);
+            int second = dp[i+secondLen] - dp[i];
+            res = Math.max(res, maxFirst + second);
+        }
+        int maxSecond = 0;
+        for(int i = secondLen; i <= n-firstLen; ++i) {
+            maxSecond = Math.max(maxSecond, dp[i] - dp[i-secondLen]);
+            int first = dp[i+firstLen] - dp[i];
+            res = Math.max(res, maxSecond + first);
         }
         return res;
-    }
-
-    private int[] buildDP(int[] nums, int len) {
-        int n = nums.length;
-        int[] dp = new int[n];
-        for(int i = 0; i < len; ++i) {
-            dp[0] += nums[i];
-        }
-        for(int i = 1; i < n-len+1; ++i) {
-            dp[i] = dp[i-1] - nums[i-1] + nums[i+len-1];
-        }
-        return dp;
     }
 }
