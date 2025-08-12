dd1781d74821c82208452d1a93e1ecb3a2bb86a8
adoumadje <adoumadjecaleb@gmail.com>
2025-08-12 21:19:04 +0200

    1105. Filling Bookcase Shelves  - top down

--- src/main/java/com/leetcode/dynamic_programming/medium/LeetCode1105.java
+++ src/main/java/com/leetcode/dynamic_programming/medium/LeetCode1105.java
@@ -1,29 +1,20 @@
 package com.leetcode.dynamic_programming.medium;
 
 public class LeetCode1105 {
-    Integer[] mem;
-    int n, shelfWidth;
-    int[][] books;
     public int minHeightShelves(int[][] books, int shelfWidth) {
-        this.books = books;
-        this.n = books.length;
-        this.shelfWidth = shelfWidth;
-        mem = new Integer[n];
-        return dfs(0);
-    }
-
-    public int dfs(int i) {
-        if(i >= n) return 0;
-        if(mem[i] != null) return mem[i];
-        mem[i] = Integer.MAX_VALUE;
-        int space = shelfWidth;
-        int maxH = Integer.MIN_VALUE;
-        for(int j = i; j < n; ++j) {
-            space -= books[j][0];
-            if(space < 0) break;
-            maxH = Math.max(maxH, books[j][1]);
-            mem[i] = Math.min(mem[i], maxH + dfs(j+1));
+        int n = books.length;
+        int[] dp = new int[n+1];
+        for(int i = 1; i <= n; ++i) {
+            int maxH = Integer.MIN_VALUE;
+            dp[i] = Integer.MAX_VALUE;
+            int space = shelfWidth;
+            for(int j = i; j > 0; --j) {
+                space -= books[j-1][0];
+                if(space < 0) break;
+                maxH = Math.max(maxH, books[j-1][1]);
+                dp[i] = Math.min(dp[i], maxH + dp[j-1]);
+            }
         }
-        return mem[i];
+        return dp[n];
     }
 }
