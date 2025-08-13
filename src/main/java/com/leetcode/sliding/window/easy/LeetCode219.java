8d77b3632527c4201e3122242044e6321c48b182
adoumadje <adoumadjecaleb@gmail.com>
2025-08-13 18:40:52 +0200

    219. Contains Duplicate II - Optimal

--- src/main/java/com/leetcode/sliding/window/easy/LeetCode219.java
+++ src/main/java/com/leetcode/sliding/window/easy/LeetCode219.java
@@ -4,17 +4,11 @@ import java.util.HashMap;
 
 public class LeetCode219 {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        if(k == 0) return false;
-        HashMap<Integer, Integer> counter = new HashMap<>();
-        int n = nums.length;
-        for(int i = 0; i < n && i <= k; ++i) {
-            counter.compute(nums[i], (x, v) -> v == null ?  1 : v + 1);
-            if(counter.get(nums[i]) > 1) return true;
-        }
-        for(int i = k+1; i < n; ++i) {
-            counter.compute(nums[i-k-1], (x, v) -> v-1);
-            counter.compute(nums[i], (x, v) -> v == null ? 1 : v + 1);
-            if(counter.get(nums[i]) > 1) return true;
+        HashMap<Integer, Integer> indexer = new HashMap<>();
+        for(int i = 0; i < nums.length; ++i) {
+            if(indexer.containsKey(nums[i]) && i - indexer.get(nums[i]) <= k)
+                return true;
+            indexer.put(nums[i], i);
         }
         return false;
     }
