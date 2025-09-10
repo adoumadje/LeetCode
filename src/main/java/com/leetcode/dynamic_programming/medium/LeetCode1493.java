e2e1b392b75ab096f18214600c96a99510ed4abb
adoumadje <adoumadjecaleb@gmail.com>
2025-09-10 06:57:24 +0200

    1493. Longest Subarray of 1's After Deleting One Element

--- src/main/java/com/leetcode/dynamic_programming/medium/LeetCode1493.java
+++ src/main/java/com/leetcode/dynamic_programming/medium/LeetCode1493.java
@@ -1,20 +1,37 @@
 package com.leetcode.dynamic_programming.medium;
 
 public class LeetCode1493 {
+//    public int longestSubarray(int[] nums) {
+//        int n = nums.length;
+//        int[][] dp = new int[2][n+2];
+//
+//        int res = 0;
+//        for(int i = 2; i < n+2; ++i) {
+//            int j = i-2;
+//            if(nums[j] == 1) {
+//                dp[0][i] = 1 + dp[0][i-1];
+//                dp[1][i] = 1 + (dp[1][i-1] == 0 ? dp[0][i-2] : dp[1][i-1]);
+//            }
+//            res = Math.max(res, Math.max(dp[0][i], dp[1][i]));
+//        }
+//
+//        return res == n ? res - 1 : res;
+//    }
+
+
     public int longestSubarray(int[] nums) {
         int n = nums.length;
-        int[][] dp = new int[2][n+2];
-
+        int l = 0;
+        int zero = 0;
         int res = 0;
-        for(int i = 2; i < n+2; ++i) {
-            int j = i-2;
-            if(nums[j] == 1) {
-                dp[0][i] = 1 + dp[0][i-1];
-                dp[1][i] = 1 + (dp[1][i-1] == 0 ? dp[0][i-2] : dp[1][i-1]);
+        for(int r = 0; r < n; ++r) {
+            if(nums[r] == 0) ++zero;
+            while(zero > 1) {
+                if(nums[l] == 0) --zero;
+                ++l;
             }
-            res = Math.max(res, Math.max(dp[0][i], dp[1][i]));
+            res = Math.max(res, r-l);
         }
-
-        return res == n ? res - 1 : res;
+        return res;
     }
 }
