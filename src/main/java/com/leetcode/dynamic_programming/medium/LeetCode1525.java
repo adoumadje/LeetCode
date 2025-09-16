c8208769f30542237ddeec3a835b6ce6f284ae7d
adoumadje <adoumadjecaleb@gmail.com>
2025-09-16 20:58:41 +0200

    1525. Number of Good Ways to Split a String - to be continued

--- src/main/java/com/leetcode/dynamic_programming/medium/LeetCode1525.java
+++ src/main/java/com/leetcode/dynamic_programming/medium/LeetCode1525.java
@@ -4,7 +4,12 @@ public class LeetCode1525 {
     public int numSplits(String s) {
         int[] alph = new int[26];
         int n = s.length();
+        int[] left = new int[n];
+        int[] right = new int[n];
         for(int i = 0; i < n; ++i) {
+            int j = n-1-i;
+            int x = s.charAt(i) - 'a';
+            int y = s.charAt(n-1-i) - 'a';
         }
         return 0;
     }
