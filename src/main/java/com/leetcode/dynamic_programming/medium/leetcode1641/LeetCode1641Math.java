ec32853055e8a60c714c3cd9f46946f8d86f3d7e
adoumadje <adoumadjecaleb@gmail.com>
2025-10-02 19:14:37 +0200

    1641. Count Sorted Vowel Strings - Maths

--- src/main/java/com/leetcode/dynamic_programming/medium/leetcode1641/LeetCode1641Math.java
+++ src/main/java/com/leetcode/dynamic_programming/medium/leetcode1641/LeetCode1641Math.java
@@ -2,6 +2,6 @@ package com.leetcode.dynamic_programming.medium.leetcode1641;
 
 public class LeetCode1641Math {
     public int countVowelStrings(int n) {
-
+        return (n+4) * (n+3) * (n+2) * (n+1) / 24;
     }
 }
