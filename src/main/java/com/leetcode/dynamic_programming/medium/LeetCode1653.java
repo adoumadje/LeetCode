9076bd7547dc2556eb8a623682366a82e12908d7
adoumadje <adoumadjecaleb@gmail.com>
2025-10-04 10:36:18 +0200

    1653. Minimum Deletions to Make String Balanced

--- src/main/java/com/leetcode/dynamic_programming/medium/LeetCode1653.java
+++ src/main/java/com/leetcode/dynamic_programming/medium/LeetCode1653.java
@@ -4,18 +4,16 @@ public class LeetCode1653 {
     public int minimumDeletions(String s) {
         char[] sa = s.toCharArray();
         int n = sa.length;
-        int[] dp = new int[n];
-        dp[0] = 1;
-        int lis = 1;
-        for(int i = 1; i < n; ++i) {
-            dp[i] = 1;
-            for(int j = i-1; j >= 0; --j) {
-                if(sa[j] <= sa[i]) {
-                    dp[i] = Math.max(dp[i], 1 + dp[j]);
-                }
-            }
-            lis = Math.max(lis, dp[i]);
+        int[] ar = new int[n];
+        for(int i = n-2; i >= 0; --i) {
+            ar[i] = sa[i+1] == 'a' ? 1 + ar[i+1] : ar[i+1];
         }
-        return n-lis;
+        int res = n;
+        int bl = 0;
+        for(int i = 0; i < n; ++i) {
+            res = Math.min(res, bl + ar[i]);
+            if(sa[i] == 'b') ++bl;
+        }
+        return res;
     }
 }
