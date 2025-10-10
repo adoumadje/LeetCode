490620b239ab167863e70694bb7b3403762bdcd7
adoumadje <adoumadjecaleb@gmail.com>
2025-10-10 21:27:51 +0200

    1937. Maximum Number of Points with Cost

--- src/main/java/com/leetcode/dynamic_programming/medium/LeetCode1937.java
+++ src/main/java/com/leetcode/dynamic_programming/medium/LeetCode1937.java
@@ -3,27 +3,30 @@ package com.leetcode.dynamic_programming.medium;
 public class LeetCode1937 {
     public long maxPoints(int[][] points) {
         int m = points.length, n = points[0].length;
-        int res = equalize(points[0]);
-        for(int r = 1; r < m; ++r) {
-            for(int c = 0; c < n; ++c) {
-                points[r][c] += points[r-1][c];
+        long[] prev = new long[n], curr;
+        long res = 0;
+        for(int[] row: points) {
+            curr = new long[n];
+            for(int i = 0; i < n; ++i) {
+                curr[i] = row[i] + prev[i];
             }
-            res = Math.max(res, equalize(points[r]));
+            res = Math.max(res, normalize(curr));
+            prev = curr;
         }
         return res;
     }
 
-    private int equalize(int[] line) {
+    private long normalize(long[] line) {
         int n = line.length;
-        int max = line[0];
+        long m = line[0];
         for(int i = 1; i < n; ++i) {
             line[i] = Math.max(line[i], line[i-1] - 1);
-            max = Math.max(max, line[i]);
+            m = Math.max(m, line[i]);
         }
         for(int i = n-2; i >= 0; --i) {
             line[i] = Math.max(line[i], line[i+1] - 1);
-            max = Math.max(max, line[i]);
+            m = Math.max(m, line[i]);
         }
-        return max;
+        return m;
     }
 }
