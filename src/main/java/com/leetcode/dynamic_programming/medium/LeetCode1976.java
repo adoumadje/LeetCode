14e0192b5737530e62addea9e25d17f6c4a36118
adoumadje <adoumadjecaleb@gmail.com>
2025-10-12 12:58:34 +0200

    1976. Number of Ways to Arrive at Destination

--- src/main/java/com/leetcode/dynamic_programming/medium/LeetCode1976.java
+++ src/main/java/com/leetcode/dynamic_programming/medium/LeetCode1976.java
@@ -1,38 +1,52 @@
 package com.leetcode.dynamic_programming.medium;
 
+import java.util.*;
+
 public class LeetCode1976 {
-    Integer[][] mem;
-    int[] nums;
-    int n;
+    public int countPaths(int n, int[][] roads) {
+        List<List<long[]>> graph = makeAdjacency(n, roads);
+        /**
+         *  [0: [dist],
+         *   1: [count]]
+         */
+        long[][] shortest = new long[n][2];
+        shortest[0][0] = 0;
+        shortest[0][1] = 1;
+        for(int i = 1; i < n; ++i) shortest[i][0] = Long.MAX_VALUE;
+        PriorityQueue<long[]> minHeap = new PriorityQueue<>(Comparator.comparingLong(a -> a[1]));
+        minHeap.offer(new long[]{0, 0});
+        long MOD = (long) (1e9 + 7);
+        while (!minHeap.isEmpty()) {
+            long[] curr = minHeap.poll();
+            int u = (int) curr[0];
+            long ut = curr[1];
+            for(long[] nei: graph.get(u)) {
+                int v = (int) nei[0];
+                long vt = nei[1];
+                long alt = ut + vt;
 
-    public int minSpaceWastedKResizing(int[] nums, int k) {
-        this.n = nums.length;
-        this.mem = new Integer[n][k+2];
-        this.nums = nums;
-        return dfs(0, k+1);
-    }
+                if(alt < shortest[v][0]) {
+                    minHeap.offer(new long[]{v, alt});
+                    shortest[v][0] = alt;
+                    shortest[v][1] = shortest[u][1];
+                } else if (alt == shortest[v][0]) {
+                    shortest[v][1] = (shortest[v][1] + shortest[u][1]) % MOD;
+                }
 
-    private int dfs(int start, int k) {
-        if(start == nums.length) return 0;
-        if(k == 0) return Integer.MAX_VALUE;
-
-        if(mem[start][k] != null) return mem[start][k];
-
-        int res = Integer.MAX_VALUE;
-        int optimalSize = 0;
-        int arrTotal = 0, currWaste;
-
-        for(int i = start; i < n; ++i) {
-            optimalSize = Math.max(optimalSize, nums[i]);
-            arrTotal += nums[i];
-            int next = dfs(i+1, k-1);
-            currWaste = next != Integer.MAX_VALUE ?
-                    optimalSize * (i-start+1) - arrTotal + next
-                    : Integer.MAX_VALUE;
-            res = Math.min(res, currWaste);
+            }
         }
-
-        return mem[start][k] = res;
+        return (int) (shortest[n-1][1] % MOD);
     }
 
+    private List<List<long[]>> makeAdjacency(int n, int[][] roads) {
+        List<List<long[]>> graph = new ArrayList<>();
+        for(int i = 0; i < n; ++i)
+            graph.add(new ArrayList<>());
+        for(int[] road: roads) {
+            int u = road[0], v = road[1], t = road[2];
+            graph.get(u).add(new long[]{v, t});
+            graph.get(v).add(new long[]{u, t});
+        }
+        return graph;
+    }
 }
