8383e42198e34f6b6f6a582dd495e8df7123f28a
adoumadje <adoumadjecaleb@gmail.com>
2025-12-31 16:11:46 +0100

    3376. Minimum Time to Break Locks I

--- src/main/java/com/leetcode/dynamic_programming/medium/LeetCode3376.java
+++ src/main/java/com/leetcode/dynamic_programming/medium/LeetCode3376.java
@@ -3,30 +3,30 @@ package com.leetcode.dynamic_programming.medium;
 import java.util.List;
 
 public class LeetCode3376 {
-    int k, n;
+    int ALL, N, K, INF = Integer.MAX_VALUE;
+    Integer[][] mem;
     List<Integer> strength;
 
     public int findMinimumTime(List<Integer> strength, int k) {
-        this.k = k;
-        this.n = strength.size();
+        this.N = strength.size();
+        this.ALL = (1<<N) - 1;
+        this.K = k;
+        this.mem = new Integer[1<<N][N*k+2];
         this.strength = strength;
-        return dfs(0, 0, 1);
+        return dfs(0, 1);
     }
 
-    private int dfs(int mask, int energy, int x) {
-        if(mask == (1<<n) - 1) return 0;
-        energy += x;
-
-        boolean cantBrake = true;
-        int res = Integer.MAX_VALUE;
-        for(int i = 0; i < n; ++i) {
-            int stren = strength.get(i);
-            if(stren <= energy && (mask & (1<<i)) == 0) {
-                cantBrake = false;
-                int newMask = mask | (1<<i);
-                res = Math.min(res, 1 + dfs(newMask, 0, x+k));
+    private int dfs(int mask, int x) {
+        if(mask == ALL) return 0;
+        if(mem[mask][x] != null) return mem[mask][x];
+        int res = INF;
+        for(int i = 0; i < N; ++i) {
+            if((mask & (1<<i)) == 0) {
+                int newMak = mask | (1<<i);
+                int cost = (strength.get(i) + x - 1) / x;
+                res = Math.min(res, cost + dfs(newMak, x+K));
             }
         }
-        return cantBrake ? 1 + dfs(mask, energy, x) : res;
+        return mem[mask][x] = res;
     }
 }
