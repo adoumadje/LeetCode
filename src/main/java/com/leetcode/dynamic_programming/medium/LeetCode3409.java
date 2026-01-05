7f2ce45b5ee923e0960f2ab6e4159136b9b01985
adoumadje <adoumadjecaleb@gmail.com>
2026-01-05 18:23:09 +0100

    3409. Longest Subsequence With Decreasing Adjacent Difference

--- src/main/java/com/leetcode/dynamic_programming/medium/LeetCode3409.java
+++ src/main/java/com/leetcode/dynamic_programming/medium/LeetCode3409.java
@@ -2,22 +2,28 @@ package com.leetcode.dynamic_programming.medium;
 
 public class LeetCode3409 {
     public int longestSubsequence(int[] nums) {
-        int n = nums.length, m = 300;
-        int[][] dp = new int[m+1][m+1];
+        int M = 300, n = nums.length;
+        int[][] dp = new int[M+1][M+1];
+        for(int i = 0; i <= M; ++i)
+            dp[nums[0]][i] = 1;
         int res = 0;
-        for (int x : nums) {
-            for (int j = 0; j <= m; ++j) {
-                if (x + j <= m)
-                    dp[x][j] = Math.max(dp[x][j], 1 + dp[x + j][j]);
-                if (x - j >= 0)
-                    dp[x][j] = Math.max(dp[x][j], 1 + dp[x - j][j]);
+        for(int i = 1; i < n; ++i) {
+            int g = nums[i];
+            for(int d = 0; d <= M; ++d) {
+                int curr = 0;
+                if(g-d > 0) {
+                    curr = Math.max(curr, 1 + dp[g-d][d]);
+                }
+                if(g+d <= M) {
+                    curr = Math.max(curr, 1 + dp[g+d][d]);
+                }
+                dp[g][d] = Math.max(dp[g][d], curr);
             }
-            int curr = 0;
-            for (int j = m; j >= 0; --j) {
-                curr = Math.max(curr, dp[x][j]);
-                dp[x][j] = curr;
+
+            for(int d = M-1; d >= 0; --d) {
+                dp[g][d] = Math.max(dp[g][d], dp[g][d+1]);
+                res = Math.max(res, dp[g][d]);
             }
-            res = Math.max(res, curr);
         }
         return res;
     }
